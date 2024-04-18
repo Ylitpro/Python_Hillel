f96f883a712cd0797078d1d3b8649fee6f1bdeaa
Ylitpro <tasyalavrik@gmail.com>
2024-04-18 21:35:19 +0300

    Add lesson 2

--- 2_Lesson/lesson.py
+++ 2_Lesson/lesson.py
@@ -4,10 +4,10 @@ print(id(a))
 number_of_pages = 123
 NumberOfPages = 123
 numberOfPages = 123
-#print(id(a))
+# print(id(a))
 x = 2345 // 1000
-print (x)
+print(x)
 x2 = 2345 % 1000
-print (x2)
+print(x2)
 x1 = 2345 // 100
-print (x1)
+print(x1)
