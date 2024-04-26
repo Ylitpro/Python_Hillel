3cb29e87b66eda560aac86cc98dbd9aced144985
Ylitpro <tasyalavrik@gmail.com>
2024-04-26 17:20:39 +0300

    Add file homework21.py

--- 3_Lesson/homework_21.py
+++ 3_Lesson/homework_21.py
@@ -1,8 +1,5 @@
 first_list = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-i = 0
-while i < len(first_list):
-    if first_list[i] == 0:
-        first_list.remove(0)
-        first_list.append(0)
-    i += 1
+for i in range(first_list.count(0)):
+    first_list.remove(0)
+    first_list.append(0)
 print(first_list)
