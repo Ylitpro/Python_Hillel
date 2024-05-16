a9c9dea60cf8e96c88fa7a36108039a9467afd3a
Ylitpro <tasyalavrik@gmail.com>
2024-05-16 16:11:07 +0300

    Change file homework73.py

--- 7_Lesson/homework73.py
+++ 7_Lesson/homework73.py
@@ -1,4 +1,4 @@
-def second_index(text: str, some_str: str) -> int or None:
+def second_index(text: str, some_str: str) -> int | None:
     ind = text.find(some_str)
     if ind >= 0:
         new_ind = text.find(some_str, ind + 1)
@@ -8,8 +8,9 @@ def second_index(text: str, some_str: str) -> int or None:
             return None
 
 
-assert second_index("sims", "s") == 3, 'Test1'
-assert second_index("find the river", "e") == 12, 'Test2'
-assert second_index("hi", "h") is None, 'Test3'
-assert second_index("Hello, hello", "lo") == 10, 'Test4'
-print('ОК')
+if __name__ == "__main__":
+    assert second_index("sims", "s") == 3, 'Test1'
+    assert second_index("find the river", "e") == 12, 'Test2'
+    assert second_index("hi", "h") is None, 'Test3'
+    assert second_index("Hello, hello", "lo") == 10, 'Test4'
+    print('ОК')
