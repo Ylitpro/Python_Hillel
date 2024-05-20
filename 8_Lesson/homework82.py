0b15ea1d79494cf5c12af72cc0df9e6fd49370dd
Ylitpro <tasyalavrik@gmail.com>
2024-05-20 15:47:23 +0300

    Change file homework82.py

--- 8_Lesson/homework82.py
+++ 8_Lesson/homework82.py
@@ -1,4 +1,4 @@
-def is_palindrome(text):
+def is_palindrome(text: str) -> bool:
     res = ''
     for i in text:
         if i.isalnum():
@@ -6,8 +6,9 @@ def is_palindrome(text):
     return res == res[::-1]
 
 
-assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
-assert is_palindrome('0P') == False, 'Test2'
-assert is_palindrome('a.') == True, 'Test3'
-assert is_palindrome('aurora') == False, 'Test4'
-print("ОК")
+if __name__ == "__main__":
+    assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+    assert is_palindrome('0P') == False, 'Test2'
+    assert is_palindrome('a.') == True, 'Test3'
+    assert is_palindrome('aurora') == False, 'Test4'
+    print("ОК")
